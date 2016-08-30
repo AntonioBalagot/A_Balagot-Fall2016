3df436d18abd33c8ef9e392fb331a1d553c8156d
AntonioBalagot <antoniobssf@yahoo.com>
2016-08-30 09:17:29 -0700

    Class Library, TempConvert Changes 8/30/16

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+/**
+ * @author APCS1
+ *
+ */
+public class Calculate {
+	public static double square(double number) {
+		double answer;
+		answer = number * number;
+		return answer;
+	}
+}
