ccec2cf1aa544027e68796a3f0953719f4219828
AntonioBalagot <antoniobssf@yahoo.com>
2016-09-26 15:06:42 -0700

    Calculate Library Final Changes Part2 9/26/16
    # Conflicts:
    #	.metadata/.log
    #	.metadata/.mylyn/.tasks.xml.zip
    #	.metadata/.mylyn/tasks.xml.zip
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/13/60fe9a89d582001615339edab43fefa7
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/17/50ef6092d582001615339edab43fefa7
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/25/80e99150da82001615339edab43fefa7
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/27/304fcb7edf82001615339edab43fefa7
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/2e/b0050bbcd682001615339edab43fefa7
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/35/10d67491df82001615339edab43fefa7
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/3c/a034c408d582001615339edab43fefa7
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/60/b06ed72ddf82001615339edab43fefa7
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/8d/e01e5b405c7b001613f2e87dfdbb0f36
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/99/809be2e7b882001611e78fb442dea941
    
    #	.metadata/.plugins/org.eclipse.core.resources/.history/cc/305adfd77a7400161f4bbddbb2022fdc
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Access
    Calculate/.indexes/e4/history.index
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Access
    Calculate/org.eclipse.jdt.core/state.dat
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Calculate
    Library/.indexes/e4/history.index
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Calculate
    Library/.markers
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Calculate
    Library/org.eclipse.jdt.core/state.dat
    #	.metadata/.plugins/org.eclipse.core.resources/.root/15.tree
    
    #	.metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.core.resources
    #	.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/remote-history/_3.fdt
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/remote-history/_3.fdx
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/remote-history/_3.frq
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/remote-history/_3.nrm
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/remote-history/_3.prx
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/remote-history/_3.tii
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/remote-history/_3.tis
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/remote-history/segments_4
    
    #	.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/server-config.json
    #	.metadata/.plugins/org.eclipse.jdt.core/2045158475.index
    #	.metadata/.plugins/org.eclipse.jdt.core/savedIndexNames.txt
    #	.metadata/.plugins/org.eclipse.m2e.logback.configuration/0.log
    #	.metadata/version.ini
    #	Calculate Library/src/Calculate.java

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -9,7 +9,7 @@
 public class DoMath {
 	public static void main(String[] args){
 		String result;
-		result = Calculate.quadForm(1, -6, 8);
+		result = Calculate.quadForm(-1, 3, 8);
 		System.out.println(result);
 	}
 }
