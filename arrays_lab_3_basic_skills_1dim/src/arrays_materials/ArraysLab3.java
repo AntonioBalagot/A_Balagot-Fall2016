11e1e7fe92e20eed71164c79df66ffb87fc24115
AntonioBalagot <antoniobssf@yahoo.com>
2016-11-06 00:23:38 -0700

    ArrayLab3 Changes 1     11/6/16

--- arrays_lab_3_basic_skills_1dim/src/arrays_materials/ArraysLab3.java
+++ arrays_lab_3_basic_skills_1dim/src/arrays_materials/ArraysLab3.java
@@ -1,3 +1,8 @@
+//Antonio Balagot APCS1
+//   11/5/16
+//5 methods that are used to manipulate elements of an array(add to, append, remove, or rotate), 
+//along with a main method to call the other 5 methods. 
+
 package arrays_materials;
 
 import java.util.Arrays;
@@ -10,12 +15,16 @@ public class ArraysLab3
 	 * in which every element is the sum of the elements at that index for the arrays arr1 and arr2. 
 	 * You can assume arrays arr1 and arr2 have at least one element each and are the same length.
 	 */
-	public static int[] sum(int[] arr1, int[] arr2)
-	{
+	//Method called sum used to add the elements at the same index for the arrays arr1 and arr2.
+	public static int[] sum(int[] arr1, int[] arr2) {
 		assert (arr1.length > 0);
 		assert (arr2.length > 0);
 		assert (arr1.length == arr2.length);
-		
+		int[] sum = new int[arr1.length];
+		for(int i = 0; i <= arr1.length - 1; i++){
+			sum[i] = arr1[i] + arr2[i];
+		}
+		return sum;
 	}
 	
 	/*
@@ -24,8 +33,9 @@ public class ArraysLab3
 	 * that consists of the elements of arr with num appended to the end.  
 	 * You can assume array arr has at least one element.
 	 */
-	public static int[] append(int[] arr, int num)
-	{
+	//Method called append used to return an array of integers of length n+1 
+	//that consists of the elements of arr with num appended to the end.
+	public static int[] append(int[] arr, int num) {
 		assert (arr.length > 0);
 		
 	}
@@ -36,8 +46,9 @@ public class ArraysLab3
 	 * except for the element at index idx (thus, the returned array has a length of arr.length – 1).  
 	 * You can assume arr has at least two elements.
 	 */
-	public static int[] remove(int[] arr, int idx)
-	{
+	//Method called remove used to return an array of integers consisting of 
+	//all of the elements of arr, except for the element at index idx
+	public static int[] remove(int[] arr, int idx) {
 		assert (arr.length >= 2);
 		
 	}
@@ -47,50 +58,42 @@ public class ArraysLab3
 	 * and returns an integer containing the sum of the elements at the even indices of arr.  
 	 * (That means elements at indices 0,2,4,6,8.)  You can assume arr has at least one element.
 	 */
-	public static int sumEven(int[] arr)
-	{
+	//Method called sumEven used to add together the elements at the even indices of arr
+	public static int sumEven(int[] arr) {
 		assert (arr.length > 0);
+		int evenSum = 0;
+		for(int i=arr.length-1; i>=1; i--) {
+			
+		}
 		
 	}
 	
-	/*
-	 * Write a method rotateRight that accepts an array of integers arr 
-	 * and does not return a value.  
-	 * The rotateRight method moves each element of arr one index to the right 
-	 * (element 0 goes to element 1, element 1 goes to element 2, …, element n-1 goes to element 0).  
-	 * You can assume arr has at least one element.
-	 */
-	public static void rotateRight(int[] arr)
-	{
+	//Method called rotateRight used to shift each element of arr one index to the right
+	public static void rotateRight(int[] arr) {
 		assert (arr.length > 0);
-		
+		int lastcell = arr[arr.length - 1];
+		for(int i = arr.length - 1; i>=1; i--){
+			arr[i] = arr[i-1];
+		}
+		arr[0] = lastcell;
 	}
 
-	/*
-	 * Write a main method that will 
-		Contain
-			integer arrays a1, a2, sumArr, appendArr, removeArr
-			int variables appendNum, removeIdx, sumOfEvens
-		Carry out the following actions
-			Declare array a1 containing the values (5, 10, 15, 20, 25, 30, 35, 40) 
-			and array a2 containing the values (7, 14, 21, 28, 35, 42, 49, 56)
-			Form sumArr by calling the sum method with a1 and a2 as inputs
-			Declare int appendNum and set it to 200.
-			Form appendArr by calling the append method with a1 and appendNum as inputs
-			Declare int removeIdx and set it to 5.
-			Form removeArr by calling remove with a2 and removeIdx
-			Form sumOfEvens by calling sumEven with appendArr as the input
-			Call rotateRight with a1 as the input
-			On their own lines print out (use Arrays.toString to transform arrays into a printable strings) :
-				sumArr
-				appendArr
-				removeArr
-				sumOfEvens (an int you don’t need Arrays.toString)
-				a1
-	 */
-	
-	public static void main(String[] args) 
-	{
+	//Main method to declare each of the methods and print their contents
+	public static void main(String[] args) {
+		int[] a1 = {5, 10, 15, 20, 25, 30, 35 ,40};
+		int[] a2 = {7, 14, 21, 28, 35, 42, 49, 56};
+		int[] sumArr = sum(a1, a2);
+		int appendNum = 200;
+		int[] appendArr = append(a1, appendNum);
+		int removeIdx = 5;
+		int[] removeArr = remove(a2, removeIdx);
+		int sumOfEvens = sumEven(appendArr);
+		rotateRight(a1);
 		
+		System.out.println(Arrays.toString(sumArr));
+		System.out.println(Arrays.toString(appendArr));
+		System.out.println(Arrays.toString(removeArr));
+		System.out.println(sumOfEvens);
+		System.out.println(Arrays.toString(a1));
 	}
 }
