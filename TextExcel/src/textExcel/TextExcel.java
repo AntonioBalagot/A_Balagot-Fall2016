1cc4ba67b9f5ca6ea7229cdc088cee00cd492d89
AntonioBalagot <antoniobssf@yahoo.com>
2017-03-03 08:42:20 -0800

    TextExcel Changes 3/3/17

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 
 // Update this file with your own code.
 
-public class TextExcel
-{
-
-	public static void main(String[] args)
-	{
+public class TextExcel{
+	Scanner userInput = new Scanner(System.in);
+	public static void main(String[] args){
+		for(int i=0 ; i<)
 	    // Add your command loop here
 	}
 }
