51523ea30ec52042dd4958e858fca7c278c62872
AntonioBalagot <antoniobssf@yahoo.com>
2017-03-30 08:54:58 -0700

    Part B Changes

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -4,19 +4,35 @@ public class FormulaCell extends RealCell {
 	private String formula;
 	public FormulaCell (String input){
 		super(input);
+		formula = input;
 	}
 	
 	public double getDoubleValue(){
+		String[] split = formula.split(" ");
+		int operator = 2;
+		double result = Double.parseDouble(split[1]);
+		for(int i = 0; i<split.length; i+=2){
+			if(operator < split.length){
+				if(split[operator].equals(("+"))){
+					double operand = Double.parseDouble(split[i+2]);
+					result = result + operand;
+					operator = operator + 2;
+				}else if(split[operator].equals(("-"))){
+					double operand = Double.parseDouble(split[i+2]);
+					result = result - operand;
+					operator = operator + 2;
+				}else if(split[operator])
+			}
+		}
+	}
 		
 	}
 	
 	public String abbreviatedCellText(){
-		getDoubleValue();
-		return ("               ").substring(0,10);
+		return (getDoubleValue() + "               ").substring(0,10);
 		
 	}
 	
 	public String fullCellText(){
-		return super.fullCellText();
-	}
+		return formula;
 }
